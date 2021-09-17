70b5a13a87301b795bf9d76af31a3b26a446e0e1
Luis Ángel Herrera Mantilla <77311495+Lmanti@users.noreply.github.com>
2021-09-16 22:45:37 -0500

    proyecto inicializado y se agregaron los modelos categoria opcion pregunta y ronda

--- Juego/JuegoPreguntas/models.py
+++ Juego/JuegoPreguntas/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.db.models.expressions import F
 
 # Create your models here.
+
+class Categoria(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+
+class Opcion(models.Model):
+    id = models.AutoField(primary_key=True)
+    descripcion = models.CharField(max_length=50)
+    escorrecta = models.BooleanField(default=False)
+
+class Pregunta(models.Model):
+    id = models.AutoField(primary_key=True)
+    descripcion = models.TextField()
+    opciones = models.ManyToManyField(Opcion, related_name='opciones')
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, default=None, null=False, blank=False, related_name='preguntas')
+
+class Ronda(models.Model):
+    id = models.AutoField(primary_key=True)
+    categoria = models.OneToOneField(Categoria, on_delete=models.CASCADE, null=False, blank=False, default=None)
+
+    
